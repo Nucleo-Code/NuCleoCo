1b96eba7a71852458cddcba8c355d77fbb4e9190
elmergustavo <elmergustavo79@gmail.com>
2022-05-24 14:24:03 -0600

    component email maquetado

--- src/Components/Countdown.tsx
+++ src/Components/Countdown.tsx
@@ -4,10 +4,86 @@ import { Countdown as TypeCountdown } from "./helpers/types";
 import { useHistory } from "react-router-dom";
 import { useState, useEffect } from "react";
 
+let countDate = 4089374000; /*  47 Days - 07 Hours - 56 Min - 14 Sec */
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
 function Countdown(props: TypeCountdown) {
-  
+  const { needButton } = props;
+  const history = useHistory();
+
+  const [currentDay, setCurrentDay] = useState(47);
+  const [currentHour, setCurrentHour] = useState(7);
+  const [currentMinute, setCurrentMinute] = useState(56);
+  const [currentSecond, setCurrentSecond] = useState(14);
+
+  const signUpHandler = () => {
+    history.push("/signup");
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      countDate = countDate - second;
+      const tempDay = Math.floor(countDate / day);
+      const tempHour = Math.floor((countDate % day) / hour);
+      const tempMinute = Math.floor((countDate % hour) / minute);
+      const tempSecond = Math.floor((countDate % minute) / second);
+
+      if (countDate < 0) {
+        clearInterval(interval);
+      } else {
+        setCurrentDay(tempDay);
+        setCurrentHour(tempHour);
+        setCurrentMinute(tempMinute);
+        setCurrentSecond(tempSecond);
+      }
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-   <></>
+    <section
+      className={`${classes["countdown-container"]} ${
+        !needButton && classes["sign-up"]
+      }`}
+    >
+      <h3>
+        Coming <span>4 Nov 2020</span>
+      </h3>
+      <div className={classes["countdown-timer"]}>
+        <div className={classes["countdown-square"]}>
+          <p className={classes.number}>{currentDay}</p>
+          <p className={classes.text}>Days</p>
+        </div>
+        <div className={classes["countdown-square"]}>
+          <p className={classes.number}>
+            {currentHour < 10 ? `0${currentHour}` : currentHour}
+          </p>
+          <p className={classes.text}>Hours</p>
+        </div>
+        <div className={classes["countdown-square"]}>
+          <p className={classes.number}>{currentMinute}</p>
+          <p className={classes.text}>Min</p>
+        </div>
+        <div className={classes["countdown-square"]}>
+          <p className={classes.number}>
+            {currentSecond < 10 ? `0${currentSecond}` : currentSecond}
+          </p>
+          <p className={classes.text}>Sec</p>
+        </div>
+      </div>
+      {needButton && (
+        <Button
+          text="Get Started"
+          className={classes.button}
+          onClick={signUpHandler}
+        />
+      )}
+    </section>
   );
 }
 
--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -3,12 +3,25 @@ import Countdown from "../Components/Countdown";
 import Form from "../Components/Form";
 import Header from "../Components/Header";
 import classes from "./SignUp.module.css";
+import imgEmail from '../assets/email.svg'
 
 function SignUp() {
   return (
     <Fragment>
       <Header />
-      
+      <main className={classes.main}>
+
+        <h1 className={classes.title}>Obten el mejor servicio de latinoamerica</h1>
+        <p className={classes["text-content"]}>
+          Puedes contactarno para poder brindarte un servicio con exelencia y calidad de software
+        </p>
+        
+        <img className={classes.countdown} src={imgEmail} alt="Logo" width="85%" />
+    
+        <div className={classes.form}>
+          <Form />
+        </div>
+      </main>
     </Fragment>
   );
 }
